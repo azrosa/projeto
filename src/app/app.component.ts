9e62e03298aa03db144c4172c62f4978cb1247eb
Alan Rosa <azevedo.rosa@hotmail.com>
2024-06-18 19:17:38 -0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'projeto';
+  arrayTarefas = [];
+
+  adicionarTarefa(tarefa: string) {
+    if (tarefa !== "") {
+      this.arrayTarefas.join(tarefa)
+    }
+    else {
+      alert("Digite algum valor.")
+    }
+  }
+
+  apagarTarefa(tarefa: string) {
+    for (let i = 0; i <= this.arrayTarefas.length; i++) {
+      if (tarefa == this.arrayTarefas[i]) {
+        this.arrayTarefas.splice(i, 1)
+      }
+    }
+  }
+
 }
